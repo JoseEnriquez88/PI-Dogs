54c9bb7eae60047e950adfbadccfa1477ae7476a
JoseEnriquez88 <eenriquez.jose@gmail.com>
2023-11-04 04:17:30 -0300

    Update & refactoring code

--- api/src/routes/routes.js
+++ api/src/routes/routes.js
@@ -1,42 +1,18 @@
-const { Router } = require('express');
-
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
-// const Dog = require('../models/Dogs');
-// const Temperament = require('../models/Temperaments');
-
-//importo los handlers
-const getBreedsDogsHandler = require('../handlers/getBreedsDogsHandler');
-const getBreedsDogsByIdHandler = require('../handlers/getBreedsDogByIdHandler');
-const getDogsByNameHandler = require('../handlers/getDogsByNameHandler');
-const postDogsHandler = require('../handlers/postDogsHandler');
-const getDogsByTemperamentsHandler = require('../handlers/getDogsByTemperamentsHandler');
-const getAllDogsHandler = require('../handlers/getAllDogsHandler');
+const { Router } = require("express");
+const getBreedsDogsHandler = require("../handlers/getBreedsDogsHandler");
+const getBreedsDogsByIdHandler = require("../handlers/getBreedsDogByIdHandler");
+const getDogsByNameHandler = require("../handlers/getDogsByNameHandler");
+const postDogsHandler = require("../handlers/postDogsHandler");
+const getDogsByTemperamentsHandler = require("../handlers/getDogsByTemperamentsHandler");
+const getAllDogsHandler = require("../handlers/getAllDogsHandler");
 
 const router = Router();
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
-
-//! GET /dogs
-router.get('/dogs', getBreedsDogsHandler);
-
-//! GET | /dogs/:idRaza
-router.get('/dogs/:id', getBreedsDogsByIdHandler);
-
-//! GET | /dogs/name?="..."
-router.get('/name', getDogsByNameHandler);
-
-//! POST | /dogs
-router.post('/dogs', postDogsHandler);
-
-//! GET | /temperaments
-router.get('/temperaments', getDogsByTemperamentsHandler);
-
-// ###
-//!GET /home
-//ruta para obtener los perros para el home en el front
-router.get('/home', getAllDogsHandler);
-
+router.get("/dogs", getBreedsDogsHandler);
+router.get("/dogs/:id", getBreedsDogsByIdHandler);
+router.get("/name", getDogsByNameHandler);
+router.post("/dogs", postDogsHandler);
+router.get("/temperaments", getDogsByTemperamentsHandler);
+router.get("/home", getAllDogsHandler);
 
 module.exports = router;
